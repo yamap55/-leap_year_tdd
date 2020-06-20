569f0daf85acf120a5786841f62e35d43df184a3
yamap55 <tetuya120@hotmail.com>
2020-06-21 00:29:42 +0900

    [add] to string process

--- leap_year/main.py
+++ leap_year/main.py
@@ -12,6 +12,17 @@ class MyYear:
         """初期化"""
         self.year = year
 
+    def __str__(self) -> str:
+        """
+        保持しているyearを文字列として返す
+
+        Returns
+        -------
+        str
+            保持しているyearの文字列表現
+        """
+        return str(self.year)
+
     def isLeap() -> bool:
         """
         うるう年かどうかを判定する
