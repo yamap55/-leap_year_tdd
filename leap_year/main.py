07df5a02b383284818958d5169a6e09875c36a99
yamap55 <tetuya120@hotmail.com>
2020-06-20 23:27:44 +0900

    directory setup

---
+++ leap_year/main.py
@@ -0,0 +1,15 @@
+"""
+うるう年
+"""
+
+
+def judgment(year: int) -> bool:
+    """
+    うるう年を判定する
+
+    Returns
+    -------
+    bool
+        うるう年か否か
+    """
+    return True
---
+++ tests/test_main.py
@@ -0,0 +1,5 @@
+from leap_year import main
+
+
+def test_judgment():
+    assert main.judgment(2000)
