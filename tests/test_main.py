786f2b286604ed8d7380587c8b05f77b8f8cf708
yamap55 <tetuya120@hotmail.com>
2020-06-21 01:02:51 +0900

    [add] unit test for year of multiple of 4

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,3 +7,13 @@ def test_save_year():
 
 def test_to_string():
     assert str(MyYear(1000)) == "1000"
+
+
+def test_is_leap_year_of_multiple_of_4():
+    target_class = MyYear(2020)
+    assert target_class.is_leap()
+
+
+def test_is_not_leap_integer_multiple_of_4():
+    target_class = MyYear(2021)
+    assert not target_class.is_leap()
